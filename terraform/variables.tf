dbdc098e959b877125410c020863b8e1215f8f8b
AndreaScorza <atlantas95@gmail.com>
2026-08-01 16:18:50 +0200

    Bootstrapping

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "project_name" {
+  description = "Project name used for tagging"
+  type        = string
+  default     = "cyberchef"
+}
